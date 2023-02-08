c072a652fff9a583a26a214cc35ef1b9a0bb2b10
Bill Turvill <turvill@hotmail.co.uk>
2023-02-08 12:29:00 +0000

    edit string to match unit test

--- test/testFunctions.php
+++ test/testFunctions.php
@@ -9,7 +9,7 @@ class testFunctions extends Testcase
     public function testDetailsString()
     {
         $elements = array("id" => "1", "name" => "Antique", "origin" => "Wales", "description" => "A description of antique", "year" => "1200", "image" => "image.jpg");
-        $expectedOutput = "<h2>Name: " . "Antique" . "</h2><p>Origin: " . "Wales" . "</p><p>Description: " . "A description of antique" . "</p><p>Year: " . "1200" . "</p>" . "<img src='images/" . "image.jpg" . "' alt='Photo of " . "Antique" . "' width='200'>";
+        $expectedOutput = "<h2>" . "Antique" . "</h2><p>Origin: " . "Wales" . "</p><p>Description: " . "A description of antique" . "</p><p>Year: " . "1200" . "</p>" . "<img src='images/" . "image.jpg" . "' alt='" . "Antique" . "' width='200'>";
         $actualOutput = detailsString($elements);
         $this->assertEquals($expectedOutput, $actualOutput);
     }
